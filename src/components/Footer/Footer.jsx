b2220aa206d87d8cde070c88e2c1a4021fd57036
Frankie Bukenya <frankie.bukenya@gmail.com>
2020-09-24 13:06:57 +0200

    Add Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.scss'
+
+const Footer = () => {
+  return(
+    <div className="Footer">
+      <small className="footer-text">Create by <a rel="noopener noreferrer" href="https://github.com/Frankie-B" target="_blank" className="footer-link">Frankie Bukenya &#169; 2020</a></small>  
+    </div>
+  )
+}
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import './Home.scss'
 
 const Home = () => {
   return (
-   <div className="Home"></div>
+    <div className="Home">
+      <h1 className="home-heading">Welcome to BrewFinder</h1>
+   </div>
   )
 }
 
