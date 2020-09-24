837b16a79574b21989988f7d66af67eadb030508
Frankie Bukenya <frankie.bukenya@gmail.com>
2020-09-24 13:43:35 +0200

    Add home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,13 @@ const Home = () => {
   return (
     <div className="Home">
       <h1 className="home-heading">Welcome to BrewFinder</h1>
+      <div className="heading-icon">
+        <i className='bx bxs-beer'></i>
+      </div>
+
+      <div className="home-content">
+        <p className="home-text">To see our list of <span> beers </span> or <span>breweries</span> follow the links in the navigation.</p>
+      </div>
    </div>
   )
 }
